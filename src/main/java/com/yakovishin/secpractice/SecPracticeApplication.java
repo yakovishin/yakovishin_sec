beca7b6160face8e5eb2a76746b7c1d7a5c7df34
yakovishinm <yakovishinm@gmail.com>
2024-03-14 17:19:23 +0300

    initial commit

---
+++ src/main/java/com/yakovishin/secpractice/SecPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.yakovishin.secpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakovishin/secpractice/SecPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakovishin.secpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
